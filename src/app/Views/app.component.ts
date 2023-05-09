d4478470e610760fcc2182927098e072fafe89c2
Affan <macbookpro2019@Affan-MacBook-Pro.local>
2023-05-09 21:44:28 +0500

    reset to initial recipe and shopping

--- src/app/Views/app.component.ts
+++ src/app/Views/app.component.ts
@@ -10,29 +10,7 @@ export class AppComponent {
 
   title = 'ChefShopping';
 
-  takeParentValue: string = 'affan';
-
-  externalRecipes : Array<{name: string, description: string}> = [{name: "", description: ""}] ;
-
-  constructor() {
-
-
-    console.log('content will change');
-    setTimeout(()=> this.takeParentValue = 'faizan'  , 2000)
-
-
-    // https://timmousk.com/blog/typescript-array-of-objects/
-    // passed data to child
-    setTimeout(()=>
-        this.externalRecipes = [
-          {name: 'biryani', description: 'sindhi biryani'},
-          {name: 'karhaai', description: 'sindhi biryani'},
-          {name: 'chaampen', description: 'sindhi biryani'},
-          {name: 'kaleji', description: 'sindhi biryani'}
-        ]
-      , 10000 )
-
-  }
+  constructor() {}
 
 
 
--- src/app/Views/recipe/recipe-list/recipe-list.component.ts
+++ src/app/Views/recipe/recipe-list/recipe-list.component.ts
@@ -6,31 +6,13 @@ import {Recipe} from "../../../Models/Recipe.model";
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements AfterContentChecked{
+export class RecipeListComponent{
 
 
   public recipes: Recipe[] = [
     new Recipe('Test recipe','This is a test recipe','https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=2048x2048&w=is&k=20&c=oo7SGfjmPkybpqoNccDsYWG-4uxSmn8G79NiLA1mNvs='),
     new Recipe('Test recipe','This is a test recipe','https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=2048x2048&w=is&k=20&c=oo7SGfjmPkybpqoNccDsYWG-4uxSmn8G79NiLA1mNvs=')
   ];
-
-  //finally reached data here from app.component -> recipe.component -> recipe-list.component 💓
-  @Input('perfectchild') externalRecipes!: Array<{name: string, description: string}>;
-
-  @Output() onstatusCreated = new EventEmitter<{statusCreated: string}>();
-  learningStatus: string = '';
-   onlearningStatus() {
-    this.onstatusCreated.emit({statusCreated: this.learningStatus})
-  }
-
-  ngAfterViewChecked(): void {
-  }
-
-  ngAfterContentChecked(): void {
-
-     console.log('recipe-list: ngAfterContentChecked called');
-
-  }
 
 
 }
